bde667d99f3a27ac8277e4c1870ff2d0761783b5
cleomartheisen <cleomartheisen@hotmail.com>
2016-12-21 11:26:24 -0200

    final
    final

---
+++ ambiente/moodle/config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodleuf_moodlebd2016';
+$CFG->dbuser    = 'moodleuf_cleomar';
+$CFG->dbpass    = 'ramoelc321';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://moodleufsmfw2016.info/moodle';
+$CFG->dataroot  = '/home/moodleuf/moodletemp';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(dirname(__FILE__) . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
